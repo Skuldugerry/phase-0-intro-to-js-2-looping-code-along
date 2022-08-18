98d3af3ab1cc5162f6c5fa8096f4f61ee1424bce
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-18 21:46:13 +0300

    first success

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const names = ["Adan","Brendan","Ali"];
+const event = (birthday);
+function writeCards(names,event){
+    debugger;
+    for (let i=0; i<names.length; i++)
+    {console.log(`Thank you,${names[i]} for the wonderful birthday gift.`)
+    debugger;}
+    const returnValue=[
+        `Thank you, ${names[0]}, for the wonderful surprise gift!`,
+        `Thank you, ${names[1]}, for the wonderful surprise gift!`,
+        `Thank you, ${names[2]}, for the wonderful surprise gift!`];
+    return returnValue;
+};
